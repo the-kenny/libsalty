f1b760170fe8fe0acc5db4177cee0bd17be84459
Moritz Ulrich <moritz@tarn-vedra.de>
2019-06-11 20:55:55 +0200

    Remove accidental backslash from src/salty_nif.c
    Co-Authored-By: Jan <ArteMisc@users.noreply.github.com>

--- src/salty_nif.c
+++ src/salty_nif.c
@@ -1662,7 +1662,6 @@ END_OK_WITH2(pk, sk);
 
 SALTY_FUNC(crypto_sign_ed25519_pk_to_curve25519, 1) DO
     SALTY_INPUT_BIN(0, pk_ed25519, crypto_sign_ed25519_PUBLICKEYBYTES);
-\
     SALTY_OUTPUT_BIN(pk_curve25519, crypto_box_curve25519xchacha20poly1305_PUBLICKEYBYTES);
 
     SALTY_CALL2(crypto_sign_ed25519_pk_to_curve25519(
